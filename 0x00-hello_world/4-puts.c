bcadfb4e5d31fded43ef1e8923f581730477d200
mimilee22 <nnejikelechi01@gmail.com>
2023-03-16 15:43:12 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ *main - this is the main function
+ *
+ *Return: 0 (success)
+*/
+ int main(void)
+{
+put("\"Programming is like building a multilingual puzzle")
+return (0);
+}
