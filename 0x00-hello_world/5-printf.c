8f3c43b7119e0901f8bd9ebd00a7dbb68c41af69
mimilee22 <nnejikelechi01@gmail.com>
2023-03-16 17:11:30 -0700

    6-size.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+
+/**
+*main - Entry point for the program
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+print("wih proper grammar, but the outcome is a piece of art");
+	return (0);
+}
