047539474117904bbc0189b13644739323145c9d
mimilee22 <nnejikelechi01@gmail.com>
2023-03-16 18:09:09 -0700

    printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
+#include<stdio.h>
 
 /**
-*main - Entry point for the program
+* main - Entry point for the program
 *
 * Return: Always 0 (Success)
 */
-
 int main(void)
 {
-print("wih proper grammar, but the outcome is a piece of art,\n");
+printf("with proper grammer, but the outcome is a piece of art,\n");
 return (0);
 }
