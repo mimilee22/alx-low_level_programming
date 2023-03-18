069ee024978730952410bbf318304a3f0bbce1c1
mimilee22 <nnejikelechi01@gmail.com>
2023-03-17 19:47:48 -0700

    print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+* main - Entry point of the program
+*
+* Return: Always 0
+*/
+
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
+
