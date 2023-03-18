f784b1bc8c458b51de3ed6ad7697e893ea92fb88
mimilee22 <nnejikelechi01@gmail.com>
2023-03-17 21:00:39 -0700

    if_else

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+* main - Entry point of the program
+*
+* Return: Always 0
+*/
+int main(void)
+{
+int  c;
+for (c = 0; c < 10; c++)
+{
+putchar(c + 48);
+}
+putchar('\n');
+return (0);
+}
