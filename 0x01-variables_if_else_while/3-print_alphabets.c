2fd3e9881bdfa763aa293b177cef5f23c2981f0b
mimilee22 <nnejikelechi01@gmail.com>
2023-03-17 19:51:52 -0700

    alphabet.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+* main - Entry point of the program
+*
+* Return: Always 0
+*/
+
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+putchar(c);
+}
+for (c = 'A'; c <= 'Z'; c++)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
