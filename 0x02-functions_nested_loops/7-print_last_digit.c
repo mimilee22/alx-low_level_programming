bf6da2b1dd71d4f99882a21643cc6cef71e38af2
mimilee22 <nnejikelechi01@gmail.com>
2023-03-23 16:00:58 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,12 +2,12 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * * print_last_digit - prints lowercase characters
- * *
- * * @num: integer parameter
- * *
- * * Return: Always 0 or 1
- * */
+* print_last_digit - prints lowercase characters
+*
+* @num: integer parameter
+*
+* Return: Always 0 or 1
+*/
 int print_last_digit(int num)
 {
 int last_digit = num % 10;
@@ -17,4 +17,4 @@ last_digit = -last_digit;
 }
 _putchar('0' + last_digit);
 return (last_digit);
-}}
+}
